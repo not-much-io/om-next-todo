7a1439d09b7babe5c1405b5cde0e4d29b777f265
not-much-io <kristo.koert@gmail.com>
2016-01-11 12:28:17 +0200

    Built basic UI with material-ui

--- src/om_next_todo/core.cljs
+++ src/om_next_todo/core.cljs
@@ -2,52 +2,47 @@
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
-            [om-next-todo.parser :as p]))
+            [om-next-todo.parser :as p]
+            [om-next-todo.om-ui-components :as ui-comp]
+            [om-next-todo.material-ui :as mui]))
 
 (enable-console-print!)
 
 (defonce app-state (atom {:app-title   "Om.Next Todo"
-                      :todos/lists [{:name "Main"
-                                     :list [{:title    "Keep an eye on bunny"
-                                             :priority 1
-                                             :inv      "Wut?"}
-                                            {:title    "Get something to eat"
-                                             :priority 3}
-                                            {:title    "Read books"
-                                             :priority 4}
-                                            {:title    "Write code"
-                                             :priority 2}]}
-                                    {:name "Secondary"
-                                     :list [{:title "Eat"
-                                             :priority 2}
-                                            {:title "Sleep"
-                                             :priority 1}
-                                            {:title "Jog"
-                                             :priority 3}]}]}))
+                          :todos [{:title    "Keep an eye on bunny"
+                                   :priority 1
+                                   :inv      "Wut?"}
+                                  {:title    "Get something to eat"
+                                   :priority 3}
+                                  {:title    "Read books"
+                                   :priority 4}
+                                  {:title    "Write code"
+                                   :priority 2}]}))
 
 (defui Root
   static om/IQuery
   (query [this]
     [:app-title
-     {:todos/lists (om/get-query ui-comp/ToDoList)}])
+     {:todos (om/get-query ui-comp/ToDoList)}])
   Object
   (render [this]
     (comment (println "RootQ: " (om/get-query this))
              (println "RootP: " (om/props this)))
-    (let [{:keys [todos/lists app-title]} (om/props this)]
-      (dom/main nil "add"))))
-
-(def test-fact (js/React.createFactory js/MaterialUI.AppBar))
-
-(defui MuiTest
-  Object
-  (render [this]
-    (test-fact #js {:title "Title"} "")))
+    (let [{:keys [todos app-title]} (om/props this)]
+      (dom/main nil
+                (mui/app-bar #js {:title app-title
+                                  :zDepth 1} "")
+                (ui-comp/todo-list todos)
+                (mui/fab #js {:style #js {:position "fixed"
+                                          :bottom "20px"
+                                          :right "20px"}}
+                         (mui/font-icon #js {:className "material-icons"}
+                                        "add"))))))
 
 (def reconciler
   (om/reconciler {:state  app-state
                   :parser p/parser}))
 
 (om/add-root! reconciler
-              MuiTest
+              Root
               (gdom/getElement "app"))
--- src/om_next_todo/om_ui_components.cljs
+++ src/om_next_todo/om_ui_components.cljs
@@ -1,7 +1,8 @@
 (ns om-next-todo.om-ui-components
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [om-next-todo.material-ui :as mui]))
 
 
 (defui ToDoItem
@@ -15,28 +16,40 @@
     (let [props (om/props this)
           title (:title props)
           priority (:priority props)]
-      )))
+      (mui/table-row nil
+                     (mui/table-row-column nil
+                                           title)
+                     (mui/table-row-column #js {:style #js {:textAlign "center"}}
+                                           priority)
+                     (mui/table-row-column nil
+                                           (mui/icon-button #js {:iconClassName "material-icons"}
+                                                            "remove"))))))
 
 (def todo-item (om/factory ToDoItem))
 
 (defui ToDoList
   static om/IQuery
   (query [this]
-    [{:list (om/get-query ToDoItem)}])
+    [{:todos (om/get-query ToDoItem)}])
   Object
   (render [this]
     (comment (println "ToDoListQ: " (om/get-query ToDoList))
              (println "ToDoListP: " (om/props this)))
-    (let [thead (dom/thead nil
-                           (dom/tr nil
-                                   (dom/th nil
-                                           "ToDo")
-                                   (dom/th #js {:className "text-center"}
-                                           "Priority")
-                                   (dom/th nil
-                                           "")))
-          props (om/props this)
-          list (sort-by :priority (:list props))]
-      )))
+    (let [thead (mui/table-header #js {:displaySelectAll false
+                                       :adjustForCheckbox false}
+                                  (mui/table-row nil
+                                                 (mui/table-header-column nil
+                                                                          "ToDo")
+                                                 (mui/table-header-column #js {:style #js {:textAlign "center"}}
+                                                                          "Priority")
+                                                 (mui/table-header-column nil
+                                                                          "")))
+          list (sort-by :priority (om/props this))]
+      (mui/paper #js {:zDepth 1
+                      :style #js {:margin 20}}
+                 (mui/table nil
+                            thead
+                            (mui/table-body nil
+                                            (map todo-item list)))))))
 
 (def todo-list (om/factory ToDoList))
